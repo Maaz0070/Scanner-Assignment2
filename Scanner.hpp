b406c2683c8d611354f13aa234f11208d6a7119b
Maaz Adil <maazadil786@gmail.com>
2022-09-01 18:41:01 -0700

    first commit

---
+++ Scanner.hpp
@@ -0,0 +1,17 @@
+//
+//  Scanner.hpp
+//  Scanner-Assignment2
+//
+//  Created by Maaz Adil on 9/1/22.
+//
+
+#ifndef Scanner_h
+#define Scanner_h
+
+class Scanner {
+    
+    public:
+        char nextToken();
+};
+
+#endif /* Scanner_h */
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Scanner-Assignment2
+//
+//  Created by Maaz Adil on 9/1/22.
+//
+
+#include <iostream>
+#include "Scanner.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
